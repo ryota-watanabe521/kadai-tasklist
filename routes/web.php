921e87551c7a8c5026b9455ce0887a343b5205d1
ryota.watanabe <ryota.w.hope.521@gmail.com>
2019-11-25 16:40:59 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+//以下は全てresourceに含まれている内容
+//CRUD
+// タスクの個別詳細ページ表示
+//Route::get('tasks/{id}', 'TasksController@show');
+// タスクの新規登録を処理
+//Route::post('tasks', 'TasksController@store');
+//　タスクの更新処理
+//Route::put('tasks/{id}', 'TasksController@update');
+//　タスクの削除
+//Route::delete('tasks/{id}', 'TasksController@destroy');
+
+//　indexはshowの補助ページ
+//Route::get('tasks', 'TasksController@index')->name('tasks.index');
+// createは新規作成用フォームページ
+//Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+//　editは更新用のフォームページ
+//Route::get('takes/{id}/edit', 'TasksController@edit')->name('tasks.edit');
